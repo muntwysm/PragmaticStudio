73471a741971d15f9e4e1231c6e30a486f9d6140
Christopher Walton <chriswalton0989@gmail.com>
2015-04-15 17:20:19 -0500

    completed video 2 & 3 exercises

---
+++ RubyProgramming_2ndGoThrough/crowdfund/crowdfund.rb
@@ -0,0 +1,9 @@
+project1_name = "N.A.S PC"
+project2_name = "Main PC Upgrades"
+project3_name = "Bathroom Fix Up!"
+
+project1_funding = 0
+
+puts "#{project1_name} has a $#{project1_funding} in funding."
+
+puts "Projects:\n\t#{project1_name}\n\t#{project2_name}\n\t#{project3_name}"
---
+++ RubyProgramming_2ndGoThrough/studio_game/studio_game.rb
@@ -0,0 +1,9 @@
+larry_name = "larry"
+larry_health = 60
+
+moe_name = "moe"
+curly_name = "curly"
+
+puts "#{larry_name}'s health is #{larry_health / 9.0}"
+
+puts "Players:\n\t#{larry_name}\n\t#{curly_name}\n\t#{moe_name}"
